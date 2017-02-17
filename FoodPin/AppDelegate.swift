f3c4ee3924f36b0bd2c606746585b2ee69b0cb53
Myron Hicks <hicks.myronj@gmail.com>
2017-02-16 20:52:57 -0600

    tab bar feature

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -24,6 +24,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let barFont = UIFont(name: "Avenir-Light", size: 24.0) {
             UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white, NSFontAttributeName:barFont]
         }
+        
+        UITabBar.appearance().tintColor = UIColor.white
+        UITabBar.appearance().barTintColor = UIColor(red: 236.0/255.0, green: 240.0/255.0, blue: 241.0/255.0, alpha: 1.0)
+        UITabBar.appearance().selectionIndicatorImage = UIImage(named: "tabitem-selected")
+
         return true
     }
 
